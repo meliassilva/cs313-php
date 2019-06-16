5ec72e2e2cc9b6127f197e69b9d4809ae7e92f78
“Mario <das16005.byui.edu>
2019-06-15 21:23:38 -0600

    Added my first node server

---
+++ web/week08/server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8888;
+
+const server = http.createServer(onRequest);
+
+
+function onRequest(req, res) {
+    if (req.url == "/home") {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.write("<h1>Welcome to the Home Page</h1>");
+        res.end();
+    } else if (req.url == "/getData") {
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+        var data = {
+            "name": "Mario Elias",
+            "class": "cs313"
+        }
+        res.write(JSON.stringify(data));
+        res.end();
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.write("<h1>Page Not Found</h1>");
+        res.end();
+    }
+}
+
+server.listen(port, hostname, () => {
+    console.log("Hello world");
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
